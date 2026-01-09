1e0aa1b1e704debeefcc1c85e509108d767b31fe
Pervonachalo <75913595+Pervonachalo@users.noreply.github.com>
2026-01-09 20:33:26 +0500

    Lafore 004

---
+++ 004.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ftemp;	// температура по Фаренгейту
+	cout << "Введите температуру по Фаренгейту: ";
+	cin >> ftemp;
+
+	int ctemp = (ftemp-32)*5 / 9;
+	cout << "Температура по Цельсию равна: " << ctemp << endl;
+
+
+	return 0;
+}
